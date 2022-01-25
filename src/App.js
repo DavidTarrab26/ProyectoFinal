90825121803ba696578b3759f0871e137219dc34
Moises Fuks <moshifuks2001@gmail.com>
2022-01-25 10:31:23 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-<<<<<<< HEAD
 import Footer from './componentes/Footer/Footer';
 import Home from './componentes/home/Home';
 //import Navbar from './componentes/navbar/Navbar';
 import UserProvaider from './store/appContext';
-=======
-import Footer from './componentes/footer';
->>>>>>> 7c7a4dad96a8a2f2c5c3580c2abb6bc5398066c4
+
 
 
 function App() {
