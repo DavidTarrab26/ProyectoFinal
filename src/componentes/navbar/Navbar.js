bdb4bf2b158065f343ad18aa5168ed34c0971be8
eitan <eitoussie@gmail.com>
2022-01-25 19:36:40 -0300

    eitan dia 2

--- src/componentes/navbar/Navbar.js
+++ src/componentes/navbar/Navbar.js
@@ -2,37 +2,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import foto from "../../assets/img/logEcomerce.png"
 import "./Navbar.css"
+
+
 const Navbar = () => {
     return ( 
-    
+   <>
+   {/* navbar */} 
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <img className='logoNav' src= {foto} alt= "logo"/>
-    
-    
-    
-      
-    <div className="navbar-collapse">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-      <li>
-        <Link to="/"><button className='botonMenu btn'>HOME</button></Link>
-        </li>
-        <li >
-        <Link  to="/pantalones"><button className='botonMenu btn' >PANTALONES</button></Link> 
-        </li>
-        <li >
-          <Link to="/bermudas"><button className='botonMenu btn'> BERMUDAS </button></Link>
-        </li>
-      </ul>
-        
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Busca tu producto" aria-label="Search"/>
-        <button className="btn botonBuscar btn-dark" type="submit">BUSCAR</button>
-      </form>
-      
-    </div>
-  </div>
-</nav>
+      <div className="container-fluid">
+        <img className='logoNav' src= {foto} alt= "logo"/>
+        <div className="navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li>
+            <Link to="/"><button className='botonMenu btn'>HOME</button></Link>
+            </li>
+            <li >
+            <Link  to="/pantalones"><button className='botonMenu btn' >PANTALONES</button></Link> 
+            </li>
+            <li >
+              <Link to="/bermudas"><button className='botonMenu btn'> BERMUDAS </button></Link>
+            </li>
+          </ul>
+            
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Busca tu producto" aria-label="Search"/>
+            <button className="btn botonBuscar btn-dark" type="submit">BUSCAR</button>
+          </form>
+          
+        </div>
+      </div>
+    </nav>
+    {/* login */}
+        <div className='login'>
+          <Link to=""><button className='btn btn-regis'> Registrate </button></Link>
+          <Link to=""><button className='btn btn-login'> Login </button></Link>
+          <Link to=""><i className="bi bi-cart4"></i> </Link>
+          </div>
+          
+</>
      );
 }
  
