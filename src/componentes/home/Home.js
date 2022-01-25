559555cbb438a54bd7ee9c17c114fc29054439dc
Moises Fuks <moshifuks2001@gmail.com>
2022-01-25 14:20:40 -0300

    tercero moshi 25/1

--- src/componentes/home/Home.js
+++ src/componentes/home/Home.js
@@ -1,20 +1,16 @@
 import Bienvenido from "../home/bienvenido";
-<<<<<<< HEAD
 import Promocion from "../home/promocion";
-=======
 import Cards from "./cards/Cards";
->>>>>>> fb4a28bf4cd3fbab190c16548c27d5732cfe0266
+
 
 
 const Home = () => {
     return ( 
         <>
             <Bienvenido />
-<<<<<<< HEAD
             <Promocion />
-=======
             <Cards />
->>>>>>> fb4a28bf4cd3fbab190c16548c27d5732cfe0266
+
         </>
      );
 }
