1c4b4ffadf9341e843d729a6816b0da0590d2164
Moises Fuks <moshifuks2001@gmail.com>
2022-01-25 12:18:39 -0300

    segundo moshi 1/25

---
+++ src/componentes/home/promocion/Promocion.js
@@ -0,0 +1,11 @@
+const Promocion = () => {
+    return ( 
+        <div>
+            <div className="d-flex text-center justify-content-center p-4">
+                <h2> OFERTAS </h2>
+            </div>
+        </div>
+     );
+}
+ 
+export default Promocion;
