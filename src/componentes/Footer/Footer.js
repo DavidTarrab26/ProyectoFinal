fb4a28bf4cd3fbab190c16548c27d5732cfe0266
Davidtarrab <dudutarrab@gmail.com>
2022-01-25 14:06:04 -0300

    david

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -49,6 +49,11 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
+            {/* Footer */}
+            <div className='d-flex justify-content-center mt-5'>
+                <p>Derechos reservados ©</p>
+                <p className='mx-2'>hecho por Moshi, Eitan y David</p>
+            </div>
         </>
      );
 }
--- src/componentes/home/Home.js
+++ src/componentes/home/Home.js
@@ -1,10 +1,12 @@
 import Bienvenido from "../home/bienvenido";
+import Cards from "./cards/Cards";
 
 
 const Home = () => {
     return ( 
         <>
             <Bienvenido />
+            <Cards />
         </>
      );
 }
