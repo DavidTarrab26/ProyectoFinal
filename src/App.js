0f78ee9e61203d120304187458d55e9784b62c62
Davidtarrab <dudutarrab@gmail.com>
2022-01-25 15:59:35 -0300

    david pantalones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './componentes/navbar/Navbar';
-import Footer from './componentes/Footer';
+import Footer from './componentes/footer';
 import Home from './componentes/home/Home';
 import UserProvaider from './store/appContext';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Pantalones from './componentes/pantalones/Pantalones';
 import Bermudas from './componentes/bermudas'
 import Admin from './componentes/admin/Admin';
