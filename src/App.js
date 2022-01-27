a76217f4fd4595c8c4f60413b0bbc927fb814f40
Davidtarrab <dudutarrab@gmail.com>
2022-01-27 18:19:32 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './componentes/navbar/Navbar';
 import Footer from './componentes/footer';
 import Home from './componentes/home';
